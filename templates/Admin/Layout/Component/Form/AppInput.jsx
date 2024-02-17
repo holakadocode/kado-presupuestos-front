f95c053998fe38a2f46125dd05fa1329b13a284f
Victor Toledo <victortoledo@worldlex.net>
2024-02-17 11:33:57 +0100

    feat: añadido AppInput (closes #8)

---
+++ templates/Admin/Layout/Component/Form/AppInput.jsx
@@ -0,0 +1,55 @@
+import { TextField } from '@mui/material';
+
+// title: Nombre que aparecera como titulo
+// placeholder: Descripcion de ayuda que aparece al pinchar en el cuadro, antes de introducidr texto
+// value: Contenido que aparecera en el cuadro en caso de que tenga valor
+// required: si es true, aparece un asterisco junto al titulo indicando que es requerido
+// disabled: si es true, apaga el componente para que no se pueda interactuar
+// isReadOnly: si es true, solo se visualizara el contenido que se introduzca en el value
+// helperText: Texto que aparecera debajo del icono, en tamaño pequeño.
+// error: cambia el color a rojo para indicar que hay un problema.
+// type="password": hace que el contenido introducido visualice asteriscos
+// fullWidth para que muestre todo en horizontal
+// color: cambia el color del cuadro y textos. Solo acepta clases de colores (warning, primary, secondary, info, etc...)
+// focused: preselecciona dicho input
+
+export default function AppInput(props) {
+  const {
+    title,
+    placeholder,
+    value,
+    required,
+    disabled,
+    helperText,
+    error,
+    type,
+    fullWidth = true,
+    isReadOnly = false,
+    color,
+    focused,
+  } = props;
+
+  return (
+    <>
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        label={title}
+        placeholder={placeholder}
+        defaultValue={value}
+        required={required}
+        disabled={disabled}
+        InputProps={{
+          readOnly: isReadOnly,
+        }}
+        helperText={helperText}
+        error={error}
+        isClearable
+        type={type}
+        fullWidth={fullWidth}
+        color={color}
+        focused={focused}
+      />
+    </>
+  );
+}
--- templates/Admin/Layout/Component/Icon/AppRemixIcons.jsx
+++ templates/Admin/Layout/Component/Icon/AppRemixIcons.jsx
@@ -1,10 +1,9 @@
 export default function AppRemixIcons(props) {
-  const { title, icon, color, className, size } = props;
+  const { title, icon, color, size } = props;
 
   return (
     <span
       title={title}
-      className={className}
       style={{
         fontSize: size,
         color: color,
