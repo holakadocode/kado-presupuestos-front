e59cc10ffb2e7444957fef1c294cd4d419436a05
Victor Toledo <victortoledo@worldlex.net>
2024-02-23 18:19:32 +0100

    feat: Añadidos enlaces y mejoras esteticas a Sidebar. Comentado almacen por fallos (cerrada #27)

--- src/Routing/RoutingScreen.jsx
+++ src/Routing/RoutingScreen.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import AdminLoginScreen from '../../templates/AdminLoginScreen';
 import AdminSidebar from '../../templates/Admin/Layout/Sidebar/AdminSidebar';
 import AdminHomeScreen from '../../templates/Admin/Screen/AdminHomeScreen';
-import AdminAlmacen from '../../templates/Admin/Screen/AdminAlmacen';
+import AdminAlmacen from '../../templates/Admin/Screen/Store/AdminAlmacen';
 import AdminProvider from '../../templates/Admin/Screen/AdminProvider';
 
 export default function RoutingScreen() {
