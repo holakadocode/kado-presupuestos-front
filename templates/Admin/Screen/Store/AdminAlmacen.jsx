433b7438b99a7d19313d62873bba227f8f237751
Jose Arozena <j.arocenap@gmail.com>
2024-03-05 17:56:14 +0100

    añadida funconalidad boton

--- templates/Admin/Screen/Store/AdminAlmacen.jsx
+++ templates/Admin/Screen/Store/AdminAlmacen.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 // //import { Route, Routes } from 'react-router-dom';
-// import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 // import Accordion from "@mui/material/Accordion";
 // import AccordionSummary from "@mui/material/AccordionSummary";
 // import AccordionDetails from "@mui/material/AccordionDetails";
@@ -13,40 +13,117 @@
 // import Modal from "@mui/material/Modal";
 import { TreeItem, TreeView } from "@mui/x-tree-view";
 import AppRemixIcons from "../../Layout/Component/Icon/AppRemixIcons";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { useState } from "react";
+import { Box, Modal, Typography } from "@mui/material";
 
 export default function AdminAlmacen() {
   const [folder, setFolder] = useState({
-    "Tornillos": ["Tornillo plano", "Tornillo estrella", "Tornillo Torx"],
-    "Tuercas": ["Tuerca redonda", "Tuerca cuadrada", "Tuerca ovalada"],
+    Tornillos: ["Tornillo plano", "Tornillo estrella", "Tornillo Torx"],
+    Tuercas: ["Tuerca redonda", "Tuerca cuadrada", "Tuerca ovalada"],
   });
 
   const FOLDERNAMES = Object.keys(folder);
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <>
-      <button className="btn btn-outline-secondary d-inline-flex align-items-center">
+     <div>
+      <button className="btn btn-outline-secondary d-inline-flex align-items-center" onClick={handleOpen}>
         <AppRemixIcons icon="ri-folder-add-line" className="me-2" />
         Nueva Carpeta
       </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Nueva Carpeta
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Formik
+                initialValues={{
+                  nombreCarpeta: "",
+                }}
+                onSubmit={(values, { resetForm }) => {
+                  handleClose();
+                  resetForm();
+                }}
+              >
+                <Form>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">
+                      Nombre
+                    </span>
+                    <Field
+                      type="text"
+                      name="nombreCarpeta"
+                      placeholder="Nombre de la carpeta"
+                      className="form-control"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Crear
+                  </button>
+                  <button type="button" className="btn btn-secondary ms-2" onClick={handleClose}>
+                    Cerrar
+                  </button>
+                </Form>
+              </Formik>
+          </Typography>
+        </Box>
+      </Modal>
+      </div>
 
       <TreeView
         aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
-        sx={{ height: "auto", flexGrow: 1, maxWidth: "auto", overflowY: "auto" }}
+        sx={{
+          height: "auto",
+          flexGrow: 1,
+          maxWidth: "auto",
+          overflowY: "auto",
+        }}
       >
         {FOLDERNAMES.map((folderName, index) => (
           <TreeItem key={index} nodeId={`folder-${index}`} label={folderName}>
-                        {folder[folderName].map((itemName, idx) => (
-                          <>
-              <TreeItem key={`${index}-${idx}`} nodeId={`item-${index}-${idx}`} label={itemName} />
-              <AppRemixIcons icon="ri-edit-line" className="me-2" />
-            <AppRemixIcons icon="ri-delete-bin-2-line" className="me-2" />
-            </>
-            ))} 
+            {folder[folderName].map((itemName, idx) => (
+              <div className="ms-5 m-2 row" key={idx}>
+                <div className="col-2">{itemName}</div>
+                <div className="col-1">
+                  <AppRemixIcons
+                    icon="ri-edit-line"
+                    className="me-2 btn btn-outline-primary btn-sm"
+                  />
+                </div>
+                <div className="col-1">
+                  <AppRemixIcons
+                    icon="ri-delete-bin-2-line"
+                    className="me-2 btn btn-outline-danger btn-sm"
+                  />
+                </div>
+              </div>
+            ))}
           </TreeItem>
         ))}
       </TreeView>
