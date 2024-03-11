b561dc7022a84358585f10d4663006ceef0bb675
Jose Arozena <j.arocenap@gmail.com>
2024-03-11 16:05:36 +0100

    update

--- templates/Admin/Layout/Component/Specific/Store/FolderAdd.jsx
+++ templates/Admin/Layout/Component/Specific/Store/FolderAdd.jsx
@@ -4,12 +4,11 @@ import { Field, Form, Formik } from "formik";
 import AppRemixIcons from "../../Icon/AppRemixIcons";
 
 export default function FolderAdd() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-const [open, setOpen] = useState(false);
-const handleOpen = () => setOpen(true);
-const handleClose = () => setOpen(false);
-
-const style = {
+  const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
@@ -23,62 +22,62 @@ const style = {
 
   return (
     <>
-<div>
-<button
-  className="btn btn-outline-secondary d-inline-flex align-items-center"
-  onClick={handleOpen}
->
-  <AppRemixIcons icon="ri-folder-add-line" className="me-2" />
-  Nueva Carpeta
-</button>
-<Modal
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <Box sx={style}>
-    <Typography id="modal-modal-title" variant="h6" component="h2">
-      Nueva Carpeta
-    </Typography>
-    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      <Formik
-        initialValues={{
-          nombreCarpeta: "",
-        }}
-        onSubmit={(values, { resetForm }) => {
-          handleClose();
-          resetForm();
-        }}
-      >
-        <Form>
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Nombre
-            </span>
-            <Field
-              type="text"
-              name="nombreCarpeta"
-              placeholder="Nombre de la carpeta"
-              className="form-control"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Crear
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary ms-2"
-            onClick={handleClose}
-          >
-            Cerrar
-          </button>
-        </Form>
-      </Formik>
-    </Typography>
-  </Box>
-</Modal>
-</div>
-</>
-  )
-    }
+      <div>
+        <button
+          className="btn btn-outline-secondary d-inline-flex align-items-center"
+          onClick={handleOpen}
+        >
+          <AppRemixIcons icon="ri-folder-add-line" className="me-2" />
+          Nueva Carpeta
+        </button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Nueva Carpeta
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <Formik
+                initialValues={{
+                  nombreCarpeta: "",
+                }}
+                onSubmit={(values, { resetForm }) => {
+                  handleClose();
+                  resetForm();
+                }}
+              >
+                <Form>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">
+                      Nombre
+                    </span>
+                    <Field
+                      type="text"
+                      name="nombreCarpeta"
+                      placeholder="Nombre de la carpeta"
+                      className="form-control"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Crear
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary ms-2"
+                    onClick={handleClose}
+                  >
+                    Cerrar
+                  </button>
+                </Form>
+              </Formik>
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+    </>
+  );
+}
